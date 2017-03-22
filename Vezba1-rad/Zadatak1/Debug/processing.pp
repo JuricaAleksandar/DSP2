83a1ad7cac03dd892c7bc917d6052a21cc3f9e41
JuricaAleksandar <jurica.aleksandar@gmail.com>
2017-03-22 11:06:13 +0100

    Vezba 2

---
+++ Vezba1-rad/Zadatak1/Debug/processing.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+processing.obj: ../processing.c
+processing.obj: ../processing.h
+processing.obj: C:/materija/RA47-2014/DSP2/Vezba1-rad/Zadatak1/c55xx_csl/inc/tistdtypes.h
+processing.obj: ../filter_coeff.h
+processing.obj: ../fir.h
+processing.obj: ../signal.h
+
+../processing.c: 
+../processing.h: 
+C:/materija/RA47-2014/DSP2/Vezba1-rad/Zadatak1/c55xx_csl/inc/tistdtypes.h: 
+../filter_coeff.h: 
+../fir.h: 
+../signal.h: 
---
+++ Vezba1-rad/Zadatak1/Debug/signal.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+signal.obj: ../signal.c
+signal.obj: ../signal.h
+signal.obj: C:/materija/RA47-2014/DSP2/Vezba1-rad/Zadatak1/c55xx_csl/inc/tistdtypes.h
+
+../signal.c: 
+../signal.h: 
+C:/materija/RA47-2014/DSP2/Vezba1-rad/Zadatak1/c55xx_csl/inc/tistdtypes.h: 
