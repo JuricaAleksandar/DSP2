1cc325385de3085681f2373840d8a644937e38f7
JuricaAleksandar <jurica.aleksandar@gmail.com>
2017-03-29 12:23:36 +0200

    zadatak

---
+++ Zadatak1rad/EQ/Debug/iir.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+iir.obj: ../iir.c
+iir.obj: ../iir.h
+iir.obj: C:/materija/RA47-2014/New\ folder/DSP2/Zadatak1rad/EQ/c55xx_csl/inc/tistdtypes.h
+
+../iir.c: 
+../iir.h: 
+C:/materija/RA47-2014/New\ folder/DSP2/Zadatak1rad/EQ/c55xx_csl/inc/tistdtypes.h: 
---
+++ Zadatak1rad/EQ/Debug/processing.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+processing.obj: ../processing.c
+processing.obj: ../processing.h
+processing.obj: C:/materija/RA47-2014/New\ folder/DSP2/Zadatak1rad/EQ/c55xx_csl/inc/tistdtypes.h
+processing.obj: ../iir.h
+
+../processing.c: 
+../processing.h: 
+C:/materija/RA47-2014/New\ folder/DSP2/Zadatak1rad/EQ/c55xx_csl/inc/tistdtypes.h: 
+../iir.h: 
